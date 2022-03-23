4baf148eb30aed98476ace5ab290dcba094b2775
fauzanpr <fauzanpr.06@gmail.com>
2022-03-23 20:19:09 +0700

    change form color in login, register, and password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header" style="background-color: rgb(255, 217, 0)"><span style="font-weight:bold;" class="center">{{ __('Login') }}</span></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
