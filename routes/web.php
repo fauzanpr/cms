d47f0e9da55d9108f8984f24014a4e8e0ce6ee4f
fauzanpr <fauzanpr.06@gmail.com>
2022-04-21 11:22:40 +0700

    Add pdf report feature

--- routes/web.php
+++ routes/web.php
@@ -159,3 +159,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('articles', ArticleController::class);
+
+Route::get('/article/cetak_pdf', [ArticleController::class, 'cetak_pdf'])->name('cetak_pdf');
